25a1a80badd4f8c9aa3af7926f6fd5f47252d78a
LPFJoao <joao.silva-coutinho@laplateforme.io>
2025-07-16 15:53:30 +0200

    string slicing

---
+++ JavaScript/String_Slicing/index.js
@@ -0,0 +1,21 @@
+// string slicing = creating a substring 
+//                            from a portion of another string
+//                            string.slice(start, end)
+
+
+const fullName = "João Silva";
+
+/*let firstName = fullName.slice(0, 4);
+let lastName = fullName.slice(5);
+let firstChar = fullName.slice(0, 1);
+let lastChar = fullName.slice(-1);
+*/
+
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(firstChar);
+console.log(lastChar);
+
+
