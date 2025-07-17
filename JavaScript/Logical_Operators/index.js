43bacc6766e78f9d30bf08d40752f70d61d80005
LPFJoao <joao.silva-coutinho@laplateforme.io>
2025-07-17 13:52:53 +0200

    Strict Equality

--- JavaScript/Logical_Operators/index.js
+++ JavaScript/Logical_Operators/index.js
@@ -4,3 +4,33 @@
 // AND = &&
 // OR  = ||
 // NOT = !
+
+// ---------- EXAMPLE 1 ----------
+const temp = 25;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is GOOD");
+}
+else{
+    console.log("The weather is BAD");
+}
+
+// ---------- EXAMPLE 2 ----------
+const temps = -250;
+
+if(temps <= 0 || temps > 30){
+    console.log("The weather is BAD");
+}
+else{
+    console.log("The weather is GOOD");
+}
+
+// ---------- EXAMPLE 3 ----------
+const isSunny = true;
+
+if(!isSunny){
+    console.log("It is CLOUDY");
+}
+else{
+    console.log("It is SUNNY");
+}
