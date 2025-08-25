6be61663c162751b905f15507abd26e6037a5d48
LFPJoao <joao.silva-coutinho@laplateforme.io>
2025-08-25 15:15:51 +0200

    Refactor database connection code in index.php and comment out router requirement

--- PHP-intro/websites/Dynamic_Web/5_MySQL_DB/index.php
+++ PHP-intro/websites/Dynamic_Web/5_MySQL_DB/index.php
@@ -2,5 +2,19 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
 
+// connect to our MYSQL DB
+
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
